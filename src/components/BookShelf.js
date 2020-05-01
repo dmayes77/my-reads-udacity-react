c4cadbe1caf2dd08564d52fed166e5ec7219c240
dmayes77 <dmayes77@gmail.com>
2020-05-01 12:56:01 -0400

    feature: added dynamic books from db

---
+++ src/components/BookShelf.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const BookShelf = (props) => {
+	let {shelfTitle, books, id} = props
+	return (
+		<div id={id} className='bookshelf'>
+			<h2 className='bookshelf-title'>{shelfTitle}</h2>
+			<div className='bookshelf-books'>
+				<ol className='books-grid'>
+					{books.map(book => (
+						<li key={book.id}>
+						<div className='book'>
+							<div className='book-top'>
+								<div
+									className='book-cover'
+									style={{
+										width: 128,
+										height: 193,
+										backgroundImage:
+											`url(${book.imageLinks.thumbnail})`,
+									}}
+								/>
+								<div className='book-shelf-changer'>
+									<select>
+										<option value='move' disabled>
+											Move to...
+										</option>
+										<option value='currentlyReading'>Currently Reading</option>
+										<option value='wantToRead'>Want to Read</option>
+										<option value='read'>Read</option>
+										<option value='none'>None</option>
+									</select>
+								</div>
+							</div>
+								<div className='book-title'>{book.title}</div>
+								<div className='book-authors'>{book.author}</div>
+						</div>
+					</li>
+					))}
+				</ol>
+			</div>
+		</div>
+	);
+};
+
+export default BookShelf;
